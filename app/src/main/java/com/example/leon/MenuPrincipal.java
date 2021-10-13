1dc1cc36edca62d35b7496323b4631372e83bc25
Fernando Jimenez Pereyra <alt9193@Saturn-I.huawei.net>
2021-10-12 22:49:05 -0500

    calidad de imagenes del menu ajustada, y formato del menu adaptable

--- app/src/main/java/com/example/leon/MenuPrincipal.java
+++ app/src/main/java/com/example/leon/MenuPrincipal.java
@@ -25,55 +25,48 @@ public class MenuPrincipal extends AppCompatActivity {
     public void openAcercaDe(View v) {
         Intent intent = new Intent(this, AcercaDe.class);
         startActivity(intent);
-        finish();
     }
 
     public void openColorearParejas(View v) {
         Intent intent = new Intent(this, Memorama.class);
         startActivity(intent);
-        finish();
     }
 
     public void openImagenesRepetidas(View v) {
         Intent intent = new Intent(this, RodearFiguraRepetida.class);
         startActivity(intent);
-        finish();
     }
 
     public void openUnirPuntos(View v) {
         Intent intent = new Intent(this, UnirPuntos_Nivel1.class);
         startActivity(intent);
-        finish();
     }
 
     public void openRodearNumero(View v) {
         Intent intent = new Intent(this, RodearNumero_Nivel1.class);
         startActivity(intent);
-        finish();
     }
 
     public void openInicio(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        finish();
+
     }
 
     public void openLaberinto(View v) {
         Intent intent = new Intent(this, Laberinto.class);
         startActivity(intent);
-        finish();
+
     }
 
     public void openSimonDice(View v) {
         Intent intent = new Intent(this, SimonDice.class);
         startActivity(intent);
-        finish();
     }
 
     public void openEncontrarSilueta(View v) {
         Intent intent = new Intent(this, primer_nivel.class);
         startActivity(intent);
-        finish();
     }
 
 }
