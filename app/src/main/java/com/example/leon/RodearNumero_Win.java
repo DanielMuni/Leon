e7e1a32c1e6ab0140f7aaa7f24883a649265134d
lperez-195 <78769060+lperez-195@users.noreply.github.com>
2021-10-17 20:13:18 -0500

    Código comentado RodearNumero_Win

--- app/src/main/java/com/example/leon/RodearNumero_Win.java
+++ app/src/main/java/com/example/leon/RodearNumero_Win.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class RodearNumero_Win extends AppCompatActivity {
+    //Se declara la variable
     TextView scoreTxt;
 
     @Override
@@ -16,11 +17,14 @@ public class RodearNumero_Win extends AppCompatActivity {
         setContentView(R.layout.activity_rodear_numero_win);
 
 
-        scoreTxt = findViewById(R.id.winScore);
+        scoreTxt = findViewById(R.id.winScore); //Se le asigna el nombre de la variable al TextView correspondiente
 
+        //Se muestra el resultado de showScore de la clase que le pertence a la pantalla  anterior
         scoreTxt.setText("Puntaje: " + RodearNumero_Nivel3.getInstance().showScore());
     }
 
+    /*Se declara la funcion openMenuPrincipal, la cual se encarga de cambiar la pantalla actual por la
+     * pantalla de la clase MenuPrincipal*/
     public void openMenuPrincipal(View v) {
         Intent intent = new Intent(this, MenuPrincipal.class);
         startActivity(intent);
